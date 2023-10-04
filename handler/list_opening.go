34fb3c16aed756f5f9d3de0a1e4e723d61e0e411
guimassoqueto <gui.massoqueto@gmail.com>
2023-10-04 19:49:42 -0300

    feat: add list openings

--- handler/list_opening.go
+++ handler/list_opening.go
@@ -2,11 +2,18 @@ package handler
 
 import (
 	"net/http"
+
 	"github.com/gin-gonic/gin"
+	"github.com/guimassoqueto/go-jobs/schemas"
 )
 
 func ListOpeningHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"msg": "list openings",
-	})
+	openings := []schemas.Opening{}
+
+	if err := db.Find(&openings).Error; err != nil {
+		sendError(ctx, http.StatusInternalServerError, "error listing openings")
+		return
+	}
+
+	sendSuccess(ctx, "Ok", openings)
 }
